fc45ae580c3ab18c30caceaa2b063125bb2c2396
wb-lx652828@alibaba-inc.com <wb-lx652828@alibaba-inc.com>
2022-10-09 16:57:54 +0800

    设置窗口的初始位置

--- app/main/windows/main.js
+++ app/main/windows/main.js
@@ -1,45 +1,51 @@
-const {BrowserWindow} = require('electron')
+const {BrowserWindow, screen} = require('electron')
 const isDev = require('electron-is-dev')
 
 
 let win
 let willQuitApp = false
 function create () {
-    win = new BrowserWindow({
-        width: 600,
-        height: 300,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    win.on('close', (e) => {
-        if(willQuitApp) {
-            win = null
-        } else {
-            e.preventDefault()
-            win.hide()
-        }
-    })
+	win = new BrowserWindow({
+		width: 600,
+		height: 300,
+		webPreferences: {
+			nodeIntegration: true, // 允许渲染进程使用node集成环境
+			contextIsolation:false,  // 隔离上下文
+			nodeIntegrationInWorker:true,
+		}
+	})
+	// 设置窗口悬浮的初始位置
+  const { left, top } = { left: screen.getPrimaryDisplay().workAreaSize.width - 400, top: screen.getPrimaryDisplay().workAreaSize.height - 250 }
+  win.setPosition(left, top)
 
-    if (isDev) {
-        win.loadURL('http://localhost:3000')
-    } else {
-        //加载配置好的页面，约定再pages下
-        win.loadFile(path.resolve(__dirname,'../../renderer/pages/main/index.html')) // 找到生产环境的path
-    }
+	win.on('close', (e) => {
+			if(willQuitApp) {
+					win = null
+			} else {
+					e.preventDefault()
+					win.hide()
+			}
+	})
+
+	if (isDev) {
+			win.loadURL('http://localhost:3000')
+	} else {
+			//加载配置好的页面，约定再pages下
+			win.loadFile(path.resolve(__dirname,'../../renderer/pages/main/index.html')) // 找到生产环境的path
+	}
 }
 
 function send(channel, ...args) {
-    win.webContents.send(channel, ...args)
+	win.webContents.send(channel, ...args)
 }
 
 function show() {
-    win.show()
+	win.show()
 }
 
 function close() {
-    willQuitApp = true
-    win.close()
+	willQuitApp = true
+	win.close()
 }
 
 module.exports = {create, send, show, close}
